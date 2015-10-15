73a8e782bfab1a5ecb0adc483bc3c1b8424aa6ed
Evan Schwartz <evan.mark.schwartz@gmail.com>
2015-10-15 15:35:17 -0700

    [TASK] Export one Pathfinder class

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-exports.Crawler = require('./lib/crawler').Crawler
-exports.Broker = require('./lib/broker').Broker
 exports.Pathfinder = require('./lib/pathfinder').Pathfinder
-exports.Subscriber = require('./lib/subscriber').Subscriber
-exports.Orchestrator = require('./lib/orchestrator').Orchestrator
