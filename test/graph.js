1e7451cb8afe2f566f316711202f31753714746c
Michael Warnock <michael@ripple.com>
2015-10-16 15:10:31 -0400

    Standard Style Cleanup + still 4 undef errors for 'describe' and 'it' which are part of Mocha

--- test/graph.js
+++ test/graph.js
@@ -1,58 +1,55 @@
-'use strict';
+'use strict'
 
-let assert = require('assert');
+let assert = require('assert')
 
-describe('Graph', function() {
-    describe('#findShortestPaths', function() {
+describe('Graph', function () {
+  describe('#findShortestPaths', function () {
+    const Graph = require('../lib/graph.js').Graph
 
-        const Graph = require('../lib/graph.js').Graph;
+    let map = {}
 
-        let map = {};
+    map['USD'] = {}
+    map['USD']['JPY'] = 1
+    map['USD']['BTC'] = 1
+    map['USD']['XRP'] = 1
+    map['USD']['TED'] = 1
+    map['TED'] = {}
+    map['TED']['ALI'] = 1
+    map['XRP'] = {}
+    map['XRP']['XAU'] = 1
+    map['XRP']['ALI'] = 1
+    map['XAU'] = {}
+    map['XAU']['ALI'] = 1
+    map['ALI'] = {}
+    map['ALI']['BOB'] = 1
+    map['ALI']['TED'] = 1
 
-        map["USD"] = {};
-        map["USD"]["JPY"] = 1;
-        map["USD"]["BTC"] = 1;
-        map["USD"]["XRP"] = 1;
-        map["USD"]["TED"] = 1;
-        map["TED"] = {};
-        map["TED"]["ALI"] = 1;
-        //map["TED"]["BOB"] = 1;
-        map["XRP"] = {};
-        map["XRP"]["XAU"] = 1;
-        map["XRP"]["ALI"] = 1;
-        map["XAU"] = {};
-        map["XAU"]["ALI"] = 1;
-        map["ALI"] = {};
-        map["ALI"]["BOB"] = 1;
-        map["ALI"]["TED"] = 1;
+    const graph = new Graph(map)
+    const paths = graph.findShortestPaths('USD', 'BOB', 5)
 
-        const graph = new Graph(map);
-        const paths = graph.findShortestPaths("USD","BOB",5);
+    it('should find 3 paths in the simple graph', function () {
+      assert.equal(paths.length, 3)
+    })
 
-        it('should find 3 paths in the simple graph', function(){
-            assert.equal(paths.length, 3);
-        });
+    const randomgraph = require('randomgraph')
 
-        const randomgraph = require('randomgraph');
+    const assetCount = 400
+    const rg = randomgraph.BarabasiAlbert(assetCount, 80, 1)
+    const bigEdgeMap = {}
+    rg.edges.forEach(function (e, i) {
+      let source = e.source
+      if (bigEdgeMap[source] === undefined) {
+        bigEdgeMap[source] = {}
+      }
+      bigEdgeMap[source][e.target] = 1
+    })
 
-        const assetCount = 400;
-        const rg = randomgraph.BarabasiAlbert(assetCount,80,1);
-        const bigEdgeMap = {};
-        rg.edges.forEach(function(e, i) {
-            let source = e.source;
-            if (bigEdgeMap[source] === undefined) {
-                bigEdgeMap[source] = {};
-            }
-            bigEdgeMap[source][e.target] = 1;
-        });
-
-        const bigGraph = new Graph(bigEdgeMap);
-        const startTime = Date.now();
-        const bgPaths = bigGraph.findShortestPaths(1,71,100);
-        console.info("found " + bgPaths.length + " paths in:" + (Date.now() - startTime));
-        it('should find at least 100 paths in the random graph', function() {
-            assert.equal(bgPaths.length,100);
-        });
-
-    });
-});
+    const bigGraph = new Graph(bigEdgeMap)
+    const startTime = Date.now()
+    const bgPaths = bigGraph.findShortestPaths(1, 71, 100)
+    console.info('found ' + bgPaths.length + ' paths in:' + (Date.now() - startTime))
+    it('should find at least 100 paths in the random graph', function () {
+      assert.equal(bgPaths.length, 100)
+    })
+  })
+})
