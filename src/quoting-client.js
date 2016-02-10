cb2294b675cb5412e8c6c46efa2df2cfa75a0c79
sentientwaffle <sentientwaffle@gmail.com>
2016-02-10 10:35:09 -0800

    [FEATURE] Support pathfinding by source amount
    Also adds tests for `QuotingClient#quotePathFrom{Destination,Source}`.
    
    Paves the way for https://github.com/interledger/five-bells-sender/issues/24.

--- src/quoting-client.js
+++ src/quoting-client.js
@@ -2,63 +2,77 @@
 
 const request = require('superagent')
 const uuid = require('uuid4')
+const _ = require('lodash')
 
 class QuotingClient {
   constructor (crawler) {
-    const _this = this
-
     this.crawler = crawler
     this.pairs = {}
-
-    this.crawler.on('pair', function (pair) {
-      _this.handleTradingPair(pair)
-    })
+    this.crawler.on('pair', this._handleTradingPair.bind(this))
   }
 
-  handleTradingPair (pair) {
+  _handleTradingPair (pair) {
     this.pairs[pair.source + ';' + pair.destination] = pair.uri
   }
 
-  * quotePathFromDestination (params, path) {
-    path = path.slice()
-
-    let payments = []
-    let destination = path.pop()
-    let destinationAccount = params.destinationAccount
-    let destinationAmount = params.destinationAmount
-    let destinationExpiryDuration = params.destinationExpiryDuration ||
-      path.length + 2
-
-    while (path.length) {
-      let source = path.pop()
-      let trader = this.pairs[source + ';' + destination]
-
-      let query = {
-        source_ledger: source,
-        destination_ledger: destination,
-        destination_account: destinationAccount,
-        destination_amount: destinationAmount,
-        destination_expiry_duration: destinationExpiryDuration
-      }
-
-      let payment = yield this.getTraderQuote(trader, query)
-      let paymentUuid = uuid()
-      payment.id = trader + '/payments/' + paymentUuid
-
-      payments.unshift(payment)
-      destination = source
-      destinationAmount = payment.source_transfers[0].credits[0].amount
-      destinationAccount = payment.source_transfers[0].credits[0].account
-      destinationExpiryDuration = payment.source_transfers[0].expiry_duration
+  quotePaths (params, paths) {
+    if (params.destination_amount) {
+      return this.quotePathsFromDestination(params, paths)
     }
-
-    return payments
+    if (params.source_amount) {
+      return this.quotePathsFromSource(params, paths)
+    }
+    throw new Error('requires sourceAmount or destinationAmount')
   }
 
   quotePathsFromDestination (params, paths) {
-    return paths.map((path) => {
-      return this.quotePathFromDestination(params, path)
-    })
+    return paths.map((path) => this.quotePathFromDestination(params, path))
+  }
+
+  quotePathsFromSource (params, paths) {
+    return paths.map((path) => this.quotePathFromSource(params, path))
+  }
+
+  * quotePathFromDestination (params, path) {
+    params = _.clone(params)
+    path = path.slice()
+    const payments = []
+    params.destination_expiry_duration = params.destination_expiry_duration || path.length + 1
+    params.destination_ledger = path.pop()
+    params.source_ledger = path.pop()
+    while (params.source_ledger) {
+      const payment = yield this.getTraderQuote(params)
+      payments.unshift(payment)
+      params = {
+        source_ledger: path.pop(),
+        destination_ledger: params.source_ledger,
+        destination_amount: payment.source_transfers[0].credits[0].amount,
+        destination_account: payment.source_transfers[0].credits[0].account,
+        destination_expiry_duration: payment.source_transfers[0].expiry_duration
+      }
+    }
+    return payments
+  }
+
+  * quotePathFromSource (params, path) {
+    params = _.clone(params)
+    path = path.slice()
+    const payments = []
+    params.source_expiry_duration = params.source_expiry_duration || 2 * path.length
+    params.source_ledger = path.shift()
+    params.destination_ledger = path.shift()
+    while (params.destination_ledger) {
+      const payment = yield this.getTraderQuote(params)
+      payments.push(payment)
+      params = {
+        source_ledger: params.destination_ledger,
+        destination_ledger: path.shift(),
+        source_amount: payment.destination_transfers[0].debits[0].amount,
+        source_account: payment.destination_transfers[0].debits[0].account,
+        source_expiry_duration: payment.destination_transfers[0].expiry_duration
+      }
+    }
+    return payments
   }
 
   /**
@@ -71,15 +85,17 @@ class QuotingClient {
    *  destination_asset
    *  destination_amount or source_amount
    */
-  * getTraderQuote (trader, quoteOpts) {
-    let res = yield request
+  * getTraderQuote (quoteOpts) {
+    const trader = this.pairs[quoteOpts.source_ledger + ';' + quoteOpts.destination_ledger]
+    const res = yield request
       .get(trader + '/quote')
       .query(quoteOpts)
     if (res.status >= 400) {
       throw new Error('Server Error: ' + res.status + ' ' + res.body)
     }
-
-    return res.body
+    const payment = res.body
+    payment.id = trader + '/payments/' + uuid()
+    return payment
   }
 }
 
